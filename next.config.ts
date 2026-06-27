aed0e1f0a10ff8adc7005253ae8e6fa09ec47e14
xmotion <94047530+xmotion-git@users.noreply.github.com>
2026-06-27 15:29:56 +0800

    transferred files out of the deleted portfolio folder
    cleaned project directory.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',          
+  output: 'export',
   images: {
-    unoptimized: true,       
+    unoptimized: true,
   },
-
 };
 
 export default nextConfig;
